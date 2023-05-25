b2614fcc18fe8f1757f28b14b810fa0ceeccd770
Alisa-Shch <132129891+Alisa-Shch@users.noreply.github.com>
2023-05-25 22:34:24 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace _23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioStolb = new System.Windows.Forms.RadioButton();
+            this.radioStroka = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioDown = new System.Windows.Forms.RadioButton();
+            this.radioUp = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Размерность (nxn):";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(327, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(94, 33);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "2";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioStolb);
+            this.groupBox1.Controls.Add(this.radioStroka);
+            this.groupBox1.Location = new System.Drawing.Point(91, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(180, 121);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Сортировка по";
+            // 
+            // radioStolb
+            // 
+            this.radioStolb.AutoSize = true;
+            this.radioStolb.Location = new System.Drawing.Point(32, 73);
+            this.radioStolb.Name = "radioStolb";
+            this.radioStolb.Size = new System.Drawing.Size(107, 29);
+            this.radioStolb.TabIndex = 1;
+            this.radioStolb.TabStop = true;
+            this.radioStolb.Text = "столбцу";
+            this.radioStolb.UseVisualStyleBackColor = true;
+            // 
+            // radioStroka
+            // 
+            this.radioStroka.AutoSize = true;
+            this.radioStroka.Checked = true;
+            this.radioStroka.Location = new System.Drawing.Point(32, 38);
+            this.radioStroka.Name = "radioStroka";
+            this.radioStroka.Size = new System.Drawing.Size(95, 29);
+            this.radioStroka.TabIndex = 0;
+            this.radioStroka.TabStop = true;
+            this.radioStroka.Text = "строке";
+            this.radioStroka.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioDown);
+            this.groupBox2.Controls.Add(this.radioUp);
+            this.groupBox2.Location = new System.Drawing.Point(293, 69);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(180, 121);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // radioDown
+            // 
+            this.radioDown.AutoSize = true;
+            this.radioDown.Location = new System.Drawing.Point(16, 73);
+            this.radioDown.Name = "radioDown";
+            this.radioDown.Size = new System.Drawing.Size(121, 29);
+            this.radioDown.TabIndex = 1;
+            this.radioDown.TabStop = true;
+            this.radioDown.Text = "убыванию";
+            this.radioDown.UseVisualStyleBackColor = true;
+            // 
+            // radioUp
+            // 
+            this.radioUp.AutoSize = true;
+            this.radioUp.Checked = true;
+            this.radioUp.Location = new System.Drawing.Point(16, 38);
+            this.radioUp.Name = "radioUp";
+            this.radioUp.Size = new System.Drawing.Size(152, 29);
+            this.radioUp.TabIndex = 0;
+            this.radioUp.TabStop = true;
+            this.radioUp.Text = "возрастанию";
+            this.radioUp.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 196);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 209);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(312, 196);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(231, 209);
+            this.textBox2.TabIndex = 5;
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSort.Location = new System.Drawing.Point(249, 276);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(57, 42);
+            this.buttonSort.TabIndex = 6;
+            this.buttonSort.Text = "Sort";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 424);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioStolb;
+        private System.Windows.Forms.RadioButton radioStroka;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioDown;
+        private System.Windows.Forms.RadioButton radioUp;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonSort;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        int n = 2;
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            n = comboBox1.SelectedIndex + 2;
+        }
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            int[,] arr = new int[n,n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i, j] = rnd.Next(10);
+                    textBox1.Text += String.Format($"{arr[i, j],-5}");
+                }
+                textBox1.Text += String.Format("\r\n");
+            }
+            textBox1.Enabled = false;
+            if (radioStroka.Checked && radioUp.Checked)
+                SortRowUp(ref arr);
+            else if (radioStroka.Checked && radioDown.Checked)
+                SortRowDown(ref arr);
+            else if (radioStolb.Checked && radioUp.Checked)
+                SortColumnUp(ref arr);
+            else if (radioStolb.Checked && radioDown.Checked)
+                SortColumnDown(ref arr);
+            textBox2.Text = null;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    textBox2.Text += String.Format($"{arr[i, j],-5}");
+                }
+                textBox2.Text += String.Format("\r\n");
+            }
+            textBox2.Enabled = false;
+        }
+        void SortRowUp(ref int[,] arr)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                flag = false;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n - 1; j++)
+                    {
+                        if (arr[i, j] > arr[i, j + 1])
+                        {
+                            int a = arr[i, j];
+                            arr[i, j] = arr[i, j + 1];
+                            arr[i, j + 1] = a;
+                            flag = true;
+                        }
+                    }
+                }
+            }
+        }
+        void SortRowDown(ref int[,] arr)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                flag = false;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n - 1; j++)
+                    {
+                        if (arr[i, j] < arr[i, j + 1])
+                        {
+                            int a = arr[i, j];
+                            arr[i, j] = arr[i, j + 1];
+                            arr[i, j + 1] = a;
+                            flag = true;
+                        }
+                    }
+                }
+            }
+        }
+        void SortColumnUp(ref int[,] arr)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                flag = false;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n - 1; j++)
+                    {
+                        if (arr[j, i] > arr[j + 1, i])
+                        {
+                            int a = arr[j, i];
+                            arr[j, i] = arr[j + 1, i];
+                            arr[j + 1, i] = a;
+                            flag = true;
+                        }
+                    }
+                }
+            }
+        }
+        void SortColumnDown(ref int[,] arr)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                flag = false;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n - 1; j++)
+                    {
+                        if (arr[j, i] < arr[j + 1, i])
+                        {
+                            int a = arr[j, i];
+                            arr[j, i] = arr[j + 1, i];
+                            arr[j + 1, i] = a;
+                            flag = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
